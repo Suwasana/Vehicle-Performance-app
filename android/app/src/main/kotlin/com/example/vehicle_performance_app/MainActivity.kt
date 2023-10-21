a4289e645167eadd4bcec201a163107a8dab4f6e
Lianage Suwasana Dammithu <suwasanaliyanage@gmail.com>
2023-10-21 23:53:32 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/vehicle_performance_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vehicle_performance_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/HomePage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).canvasColor,
+        title: const Text("My Vehicle Records"),
+        centerTitle: true,
+        foregroundColor: Colors.white,
+        leading:IconButton(
+                onPressed: (){print("menu");}, 
+                icon: const Icon(Icons.menu)),
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: (){print("add record");}, 
+                icon: const Icon(Icons.add)),],
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
